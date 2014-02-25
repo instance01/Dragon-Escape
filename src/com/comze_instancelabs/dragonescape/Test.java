58de9d15f139b6c59903f473aadf12fc2eed9795
InstanceLabs <danielratke97@hotmail.de>
2014-02-25 19:15:39 +0100

    adds dragon yaw

--- src/com/comze_instancelabs/dragonescape/Test.java
+++ src/com/comze_instancelabs/dragonescape/Test.java
@@ -24,9 +24,13 @@ public class Test extends EntityEnderDragon {
 	private double X;
 	private double Y;
 	private double Z;
-
-	public Test(Location loc, World world, ArrayList<Vector> p) {
+	private Main m;
+	private String arena;
+	
+	public Test(Main m, String arena, Location loc, World world, ArrayList<Vector> p) {
 		super(world);
+		this.m = m;
+		this.arena = arena;
 		currentid = 0;
 		this.points = p;
 		setPosition(loc.getX(), loc.getY(), loc.getZ());
@@ -107,6 +111,7 @@ public class Test extends EntityEnderDragon {
 				currentid += 1;
 			} else {
 				// finish
+				m.stop(m.h.get(arena), arena);
 			}
 
 			double disX = (this.locX - points.get(currentid).getX());
@@ -118,9 +123,61 @@ public class Test extends EntityEnderDragon {
 			this.X = (Math.abs(disX) / tick_);
 			this.Y = (Math.abs(disY) / tick_);
 			this.Z = (Math.abs(disZ) / tick_);
+			
+			if (this.locX < points.get(currentid).getX()) {
+				if (this.locZ > points.get(currentid).getZ()) {
+					System.out.println("A");
+					this.yaw = getLookAtYaw(new Vector(this.X, this.Y, this.Z)) + 180F;
+				} else {
+					System.out.println("B");
+					this.yaw = getLookAtYaw(new Vector(this.X, this.Y, this.Z)) - 90F;
+				}
+			} else { // (this.locX > points.get(currentid).getX())
+				if (this.locZ > points.get(currentid).getZ()) {
+					this.yaw = getLookAtYaw(new Vector(this.X, this.Y, this.Z)) + 90F;
+				} else {
+					this.yaw = getLookAtYaw(new Vector(this.X, this.Y, this.Z));
+				}
+			}
+
 		}
 
+		if (this.locX < points.get(currentid).getX()) {
+			if (this.locZ > points.get(currentid).getZ()) {
+				System.out.println("A");
+				this.yaw = getLookAtYaw(new Vector(this.X, this.Y, this.Z)) + 180F;
+			} else {
+				System.out.println("B");
+				this.yaw = getLookAtYaw(new Vector(this.X, this.Y, this.Z)) - 90F;
+			}
+		} else { // (this.locX > points.get(currentid).getX())
+			if (this.locZ > points.get(currentid).getZ()) {
+				this.yaw = getLookAtYaw(new Vector(this.X, this.Y, this.Z)) + 90F;
+			} else {
+				this.yaw = getLookAtYaw(new Vector(this.X, this.Y, this.Z));
+			}
+		}
+		
 		return new Vector(tempx, tempy, tempz);
 	}
+	
+	
+	public static float getLookAtYaw(Vector motion) {
+        double dx = motion.getX();
+        double dz = motion.getZ();
+        double yaw = 0;
+
+        if (dx != 0) {
+            if (dx < 0) {
+                yaw = 1.5 * Math.PI;
+            } else {
+                yaw = 0.5 * Math.PI;
+            }
+            yaw -= Math.atan(dz / dx);
+        } else if (dz < 0) {
+            yaw = Math.PI;
+        }
+        return (float) (-yaw * 180 / Math.PI - 90);
+    }
 
 }
